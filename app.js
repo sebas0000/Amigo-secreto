3b6ee2f5c9c68f2268671ac930ed40cf4b605859
sebas0000 <sebastiangomezcalvario@gmail.com>
2025-03-18 00:09:40 -0600

    creacion del js y modificaciones para el uso correcto del desafio de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let amigo of amigos) {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
+
